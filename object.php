8c5a6771fd38f13335c26764fe2276ec9c00cb57
Adam Burroughs <adam.burroughs01@gmail.com>
2018-02-08 10:14:59 -0700

    created a database powered dynamic object page!

--- object.php
+++ object.php
@@ -1,45 +1,57 @@
 <?php
+
+// Retrieve id from URL
 $id = $_REQUEST['id'];
 
+
+// Set-up empty object array in case no object is found for id given
 $object = array(
-	'title' => '',
-	'description' => '',
+	'name' => '',
+	'cook_time' => '',
 	'ingredients' => '',
 	'instructions' =>'',
  );
 
 
-// set the object variables 
-//based on the id value from the URL
-if ($id ==1){
-	$object = array(
-		'title' => 'Recipe #1',
-		'description' => 'Zavioli with Spinach and Ricotta',
-		'ingredients' => '1 1/4 cups ricotta cheese
-					1 cup chopped spinach
-					1 extra large zucchini',
-		'instructions' => 'Preheat oven to 350 degrees F (175 degrees C)
-					Mix ricotta cheese, spinach, egg, and pepper together in a bowl',
-	);
-}
+// Database connection credentials 
+$servername ='localhost';
+$username = 'homestead';
+$password = 'secret';
+
+
+// Create connection
+$connection = new mysqli($servername, $username, $password);
+
+// Check for an error
+if ($connection->connect_error){
+	echo 'Connection failed:' . $connection->connect_error;
+	exit;
+} 
+
+// Otherwise connected successfully
+//echo 'Connected successfully!';
+
+// Connect to the "fitl" database
+$connection->select_db('fitl');
+
+// Query to select object
+// SELECT * FROM recipes WHERE id = 1
+$sql = 'SELECT * FROM Recipe WHERE id = ' .$id;
+
+// Execute the query 
+$result = $connection->query($sql);
+
+// Check for and retrieve the object
+if ($result->num_rows > 0){
 	
-
-elseif ($id ==2){
-  $object = array(
-  		'title' => 'Recipe #2',
-  		'description' => 'Guacamole',
-  		'ingredients' => '3 avocados - peeled, pitted, and mashed 
-					      1 lime, juiced  3 tablespoons chopped fresh cilantro',
-		'instructions' => 'In a medium bowl, mash together the avocados, 
-		                   lime juice, and salt. Mix in onion, cilantro, tomatoes, 
-		                   and garlic. Stir in cayenne pepper. Refrigerate 1 hour 
-		                   for best flavor, or serve immediately',
-
-
-
-  );
-	
+	// Store objects information in $object
+	$object = $result->fetch_assoc();
+	//echo '<pre>';
+	//print_r($object);
+	//echo '</pre>';
 }
+
+
 
 
 ?>
@@ -47,13 +59,14 @@ elseif ($id ==2){
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php echo $object['title']; ?></title>
+		<title><?php echo $object['name']; ?></title>
 	</head>
 	
 
 
 	<body>
-		<h1><?php echo $object['description']; ?></h1>
+		<h1><?php echo $object['name']; ?></h1>
+		<p><?php echo $object['cook_time']; ?></p>
 		<p><?php  echo $object['ingredients']; ?></p>
 		<p><?php  echo $object['instructions']; ?></p>
 
